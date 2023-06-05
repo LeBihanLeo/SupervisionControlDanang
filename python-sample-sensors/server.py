178b89b1aedcf74950a9162a24a5155831fb0d4c
Florian Latapie <florian.latapie@etu.unice.fr>
2023-06-05 14:56:03 +0700

    example capteur 1 avec valeurs aléatoires

--- python-sample-sensors/server.py
+++ python-sample-sensors/server.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/example', methods=['GET'])
 def get_value():
-    data = {"value": "10.0"}
+    data = {\
+                "solar_panel_1":{\
+                    "is_on": True,\
+                    "power": 100,\
+                    "voltage": random.randint(100, 200),\
+                    "current": 100,\
+                }\
+            }
+  
     return jsonify(data)
 
 if __name__ == '__main__':
