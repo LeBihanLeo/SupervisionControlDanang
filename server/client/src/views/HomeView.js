0e87a38baaf1e3be027f5dbf0b30b89434627991
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-06-08 10:32:47 +0700

    add card class

--- server/client/src/views/HomeView.js
+++ server/client/src/views/HomeView.js
@@ -3,7 +3,7 @@ import MenuBarComponent from "../components/MenuBarComponent";
 
 function HomeView() {
     return (
-        <div style={{ padding: 20 }} className={"white-card"}>
+        <div style={{ padding: 20 }}>
           <MenuBarComponent></MenuBarComponent>
           <h2>Home View</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
