90a79f2af70f60dca9e65f538ad252a4d5b1c132
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-06-08 14:07:13 +0700

    fix

--- UI_Server/client/src/views/DashboardView.js
+++ UI_Server/client/src/views/DashboardView.js
@@ -1,27 +1,19 @@
 import LineChart from "../components/AreaChartComponent";
+import AreaChartComponent from "../components/AreaChartComponent";
 
 function DashboardView() {
     return (
         <div style={{ padding: 20 }}>
-           <div id={"big-center-card"}>
-               <div className={"white-card"}>
-                   <LineChart></LineChart>
-               </div>
-               <div className={"white-card"}>
-                   <LineChart></LineChart>
-               </div>
-               <div className={"white-card"}>
-                   <LineChart></LineChart>
-               </div>
-               <div className={"white-card"}>
-                   <LineChart></LineChart>
-               </div>
-               <div className={"white-card"}>
-                   <LineChart></LineChart>
-               </div>
+           <div id={"big-center-card"} class={"container"}>
+                   <AreaChartComponent className="item"></AreaChartComponent>
+                   <AreaChartComponent className="item"></AreaChartComponent>
+                   <AreaChartComponent className="item"></AreaChartComponent>
+                   <AreaChartComponent className="item"></AreaChartComponent>
+                   <AreaChartComponent className="item"></AreaChartComponent>
            </div>
         </div>
     );
   }
 
 export default DashboardView;
+
