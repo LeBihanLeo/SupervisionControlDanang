f28c3cca5b6f92560bd23ce07fa48a3ce9c544ba
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-06-08 11:53:07 +0700

    ca avance

--- UI_Server/client/src/views/HomeView.js
+++ UI_Server/client/src/views/HomeView.js
@@ -1,15 +1,21 @@
 import MenuBarComponent from "../components/MenuBarComponent";
-
+import PieChart from "../components/PieChart";
+import {useState} from "react";
+import {Data} from "../utils/Data";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import BarChartComponent from "../components/BarChartComponent";
+import LineChart from "../components/AreaChartComponent";
 
 function HomeView() {
     return (
         <div style={{ padding: 20 }}>
           <MenuBarComponent></MenuBarComponent>
-          <h2>Home View</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-        <div className={"white-card"}>
-
-        </div>
+           <div id={"big-center-card"}>
+               <div className={"white-card"}>
+                   <LineChart></LineChart>
+               </div>
+           </div>
         </div>
     );
   }
