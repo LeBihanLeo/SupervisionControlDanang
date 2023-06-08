4c9a32df726d7731d1102e0bbe964ec8dd33c3f6
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-06-08 16:38:44 +0700

    aller

--- UI_Server/client/src/components/MenuBarComponent.js
+++ UI_Server/client/src/components/MenuBarComponent.js
@@ -1,13 +1,13 @@
-import MenuItemComponent from './MenuItemComponent';
-import HomeIcon from '@mui/icons-material/Home';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ViewInArIcon from '@mui/icons-material/ViewInAr';
-import CloseIcon from '@mui/icons-material/Close';
+import  MenuItemComponent from './MenuItemComponent';
+import HomeIcon from '../ressources/icon/home.png';
+import DashboardIcon from '../ressources/icon/dashboard.png';
+import ViewInArIcon from '../ressources/icon/visualize.png';
+import CloseIcon from '../ressources/icon/cross.png';
 
 const MenuBarComponent = () => {
     return (
         <div className='menu-bar' >
-            <MenuItemComponent icon={HomeIcon} text={'Home'} />
+            <MenuItemComponent icon={HomeIcon} labelStyle={{ fontSize: '200%' }} text={'Home'} />
             <MenuItemComponent icon={DashboardIcon} text={'Dashboard'} />
             <MenuItemComponent icon={ViewInArIcon} text={'Overview'} />
             <MenuItemComponent icon={CloseIcon} text={'Disconnect'} />
--- UI_Server/client/src/components/MenuItemComponent.js
+++ UI_Server/client/src/components/MenuItemComponent.js
@@ -1,10 +1,11 @@
 const MenuItemComponent = ({icon, text}) => {
-    const DiplayedIcon = icon;
 
     return (
         <div className="menu-item-box" >
             <div className="menu-item-box-data">
-                <DiplayedIcon className="menu-item-icon" color="#0047FF" />
+                <div className="image-container">
+                    <img src={icon}/>
+                </div>
                 <div className="menu-item-box-text">{text}</div>
             </div>
             <div className="circle" />
