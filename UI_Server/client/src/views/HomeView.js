0193bf4fd83c70184c92eda64b61e96474e344b4
Benoit Gaudet <bgaudet38@gmail.com>
2023-06-08 15:43:05 +0700

    Update menu bar css

--- UI_Server/client/src/views/HomeView.js
+++ UI_Server/client/src/views/HomeView.js
@@ -8,10 +8,8 @@ import LineChart from "../components/AreaChartComponent";
 
 function HomeView() {
     return (
-      <div>
-        
+      <div className="centered-view">
         <MenuBarComponent/>
-        <div style={{ padding: 20 }} id={"horizontal-page"}>
           <div id={"big-center-card"}>
             <div className={"white-card"} id={"general-chart-home"}>
               <LineChart></LineChart>
@@ -19,21 +17,20 @@ function HomeView() {
             <div className={"white-card"} id={"general-data-home"}>
             <div>
               Total energie produced:
-              <div class = "numero-home">
+              <div className = "numero-home">
                 456 W
               </div>
               Energie needed
-              <div class = "numero-home">
+              <div className = "numero-home">
                 4 W
               </div>
               Connected devices
-              <div class = "numero-home">
+              <div className = "numero-home">
                 5 devices
               </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
     );
   }
