946e854baedd75152821298541105715e23b4184
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-06-09 11:11:27 +0700

    fix

--- UI_Server/client/src/components/MenuItemComponent.js
+++ UI_Server/client/src/components/MenuItemComponent.js
@@ -2,21 +2,23 @@ const MenuItemComponent = ({icon, text, isActivated}) => {
 
     if (isActivated) {
         return (
-            <div className="menu-item-box-activated" >
+            <div className="menu-item-box menu-item-box-activated" >
                 <div className="menu-item-box-data">
-                    <div className="image-container">
+                    <div className="image-container-selected">
                         <img src={icon}/>
                     </div>
                     <div className="menu-item-box-text">{text}</div>
                 </div>
-                <div className="circle-activated" />
+                <div className="circle circle-activated" />
             </div>
         );
     } else {
         return (
             <div className="menu-item-box" >
                 <div className="menu-item-box-data">
-                    <DiplayedIcon className="menu-item-icon" />
+                    <div className="image-container">
+                        <img src={icon}/>
+                    </div>
                     <div className="menu-item-box-text">{text}</div>
                 </div>
                 <div className="circle" />
