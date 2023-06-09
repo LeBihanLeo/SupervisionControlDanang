8b94932188619ec4e12dba1ce41612e0625e2db5
Benoit Gaudet <bgaudet38@gmail.com>
2023-06-09 12:11:09 +0700

    Fix bug color icon

--- UI_Server/client/src/components/MenuItemComponent.js
+++ UI_Server/client/src/components/MenuItemComponent.js
@@ -4,10 +4,10 @@ import {Link} from "react-router-dom";
 const MenuItemComponent = ({icon, text, isActivated, link}) => {
 
     return (
-        <Link to={link} style={{ textDecoration: 'none' }}>
+        <Link to={link} className={'menu-link'}>
             <div className={"menu-item-box" + (isActivated ? " menu-item-box-activated" : "")} >
                 <div className="menu-item-box-data">
-                    <div className="image-container image-container-selected">
+                    <div className={"image-container" + (isActivated ? " image-container-activated" : "")}>
                         <img src={icon}/>
                     </div>
                     <div className="menu-item-box-text">{text}</div>
