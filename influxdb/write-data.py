c4751aaa2e64518c8e6946ea10adb7ace8d336e6
Benoit Gaudet <bgaudet38@gmail.com>
2023-06-14 10:47:53 +0700

    Update docker-compose

---
+++ influxdb/write-data.py
@@ -0,0 +1,12 @@
+bucket="ss6"
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+   
+for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org="dniit", record=point)
+  time.sleep(1) # separate points by 1 second
