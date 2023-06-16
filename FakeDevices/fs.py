477f40b1f92da29ac8d62a1448a769bf5124b111
Thomas <billequin.thomas@gmail.com>
2023-06-16 15:40:07 +0700

    add fake device code

--- FakeDevices/fs.py
+++ FakeDevices/fs.py
@@ -1,6 +1,26 @@
 import subprocess
+from threading import Thread
+import random
+import json
 
-command = 'mosquitto_pub -h localhost -p 1883 -m "TA GUEULE THOMAS" -t topic_test/test2'
+commandStart = 'mosquitto_pub -h localhost -p 1883 -m "' 
+commandEnd = '" -u test -P test'
 
-for _ in range(10):
-    subprocess.call(command, shell=True)
+numberOfThreads = 5
+
+def device(i, name):
+    print(name + " device " + str(i) + " start")
+    for j in range (10):
+        command = commandStart + str(random.randint(1, 100)) + commandEnd + ' -t ' + name +str(i+1)+'/prod'
+        subprocess.call(command, shell=True)
+
+for i in range(numberOfThreads):
+    mon_thread = Thread(target=device, args=(i, "sp"))
+    mon_thread.start()
+    mon_thread = Thread(target=device, args=(i, "wm"))
+    mon_thread.start()
+    
+for i in range(numberOfThreads*2):  
+    mon_thread.join()
+
+print("threads finis.")
