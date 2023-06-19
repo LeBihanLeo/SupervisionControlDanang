39590e45b5f721deca488059c345dda3c0d0d514
Thomas <billequin.thomas@gmail.com>
2023-06-19 11:50:08 +0700

    new python code to generate data

--- FakeDevices/fs.py
+++ FakeDevices/fs.py
@@ -2,25 +2,24 @@ import subprocess
 from threading import Thread
 import random
 import json
+import time
 
 commandStart = 'mosquitto_pub -h localhost -p 1883 -m "' 
 commandEnd = '" -u test -P test'
 
-numberOfThreads = 5
+names = ["sp", "wm"]
+
+numberOfThreads = 10
+
 
 def device(i, name):
     print(name + " device " + str(i) + " start")
-    for j in range (10):
+    for j in range (999999):
         command = commandStart + str(random.randint(1, 100)) + commandEnd + ' -t ' + name +str(i+1)+'/prod'
         subprocess.call(command, shell=True)
+        time.sleep(1)
+
 
 for i in range(numberOfThreads):
-    mon_thread = Thread(target=device, args=(i, "sp"))
+    mon_thread = Thread(target=device, args=(i, names[i%len(names)]))
     mon_thread.start()
-    mon_thread = Thread(target=device, args=(i, "wm"))
-    mon_thread.start()
-    
-for i in range(numberOfThreads*2):  
-    mon_thread.join()
-
-print("threads finis.")
