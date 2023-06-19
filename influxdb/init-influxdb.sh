d5e17feb21b88175ca52e04c62af96c2337e1015
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-06-19 16:15:12 +0700

    influx db work

---
+++ influxdb/init-influxdb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DOCKER_INFLUXDB_INIT_HOST=$DOCKER_INFLUXDB_INIT_HOST
+export DOCKER_INFLUXDB_OPENHAB_BUCKET=$DOCKER_INFLUXDB_OPENHAB_BUCKET
+
+winpty docker exec -it influxdb influx bucket create -n openhab -o dniit -r 72h
+
