e2413dba96d89198a825b76cee33b73634bd0c0b
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-06-20 10:50:48 +0700

    espoir

--- influxdb/init-influxdb.sh
+++ influxdb/init-influxdb.sh
@@ -2,10 +2,5 @@
 
 export DOCKER_INFLUXDB_INIT_HOST=$DOCKER_INFLUXDB_INIT_HOST
 export DOCKER_INFLUXDB_OPENHAB_BUCKET=$DOCKER_INFLUXDB_OPENHAB_BUCKET
+winpty docker exec -it influxdb influx bucket create -n openhab -o dniit -r 72h
 
-#if os is windows (MINGW64_NT*) then use winpty
-if [ "$(uname)" == "MINGW64_NT"* ]; then
-  winpty docker exec -it influxdb influx bucket create -n openhab -o dniit -r 72h
-else
-  docker exec -it influxdb influx bucket create -n openhab -o dniit -r 72h
-fi
