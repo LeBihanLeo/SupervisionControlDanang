86dcb27b6b845b01b9684641f4527a58b7486008
Benoit Gaudet <bgaudet38@gmail.com>
2023-06-27 17:08:07 +0700

    update device generator. Possibility to add easly one device

--- openhab_config_generartor/openhab_files_helper.py
+++ openhab_config_generartor/openhab_files_helper.py
@@ -2,23 +2,43 @@ import re
 
 THINGS_FILE_PATH = "../openhab/conf/things/test-broker.things"
 ITEMS_FILE_PATH = "../openhab/conf/items/test-broker.items"
-THING_NAME = "mqtt_thing_data"
-THING_HEADER = "Bridge mqtt:broker:mosquitto [ host=\"mqtt\", port=8883,  secure=false, username=\"admin\", password=\"admin123\" ] {"
+THING_NAME = "mqtt_thing_data"  # not used for the moment
 
 
 def add_new_device(device_type, device_location, device_id):
-    print("e")
+    device_name = f"{device_type}_{device_location}_{device_id}"
+    device_topic = f"{device_location}/{device_type}{device_id}"
+    add_new_thing(device_name, device_topic)
+    add_new_item(device_name)
+
+
+def write_in_file(filepath, data):
+    f = open(filepath, "w")
+    f.write(data)
+    f.close()
+
 
 def add_new_thing(device_name, device_topic):
     f = open(THINGS_FILE_PATH, "r")
     data = f.read()
+    f.close()
     data = re.split("\s*}\s*}$", data)
-    data = data[0]\
-           + "\n\n"\
-           + f"            Type number : {device_name} \"{device_name} channel\" [ stateTopic=\"{device_topic}\", transformationPattern=\"JSONPATH:$.power\" ]\n" \
-           + f"            Type string : {device_name} \"{device_name} channel\" [ stateTopic=\"{device_topic}\", transformationPattern=\"JSONPATH:$.location\" ]\n" \
-           + f"            Type number : {device_name} \"{device_name} channel\" [ stateTopic=\"{device_topic}\", transformationPattern=\"JSONPATH:$.timestamp\" ]\n" \
+    data = data[0] \
+           + "\n\n" \
+           + f"            Type number : {device_name}_power_channel \"{device_name} channel\" [ stateTopic=\"{device_topic}\", transformationPattern=\"JSONPATH:$.power\" ]\n" \
+           + f"            Type string : {device_name}_location_channel \"{device_name} channel\" [ stateTopic=\"{device_topic}\", transformationPattern=\"JSONPATH:$.location\" ]\n" \
+           + f"            Type number : {device_name}_timestamp_channel \"{device_name} channel\" [ stateTopic=\"{device_topic}\", transformationPattern=\"JSONPATH:$.timestamp\" ]\n" \
            + "    }\n}"
-    print(data)
+    write_in_file(THINGS_FILE_PATH, data)
 
-add_new_thing("sp_test","sptest/test")
+
+def add_new_item(device_name):
+    f = open(ITEMS_FILE_PATH, "r")
+    data = f.read()
+    f.close()
+    data = data \
+           + "\n\n" \
+           + "Number " + device_name + "_power \"" + device_name + " power\" { channel=\"mqtt:topic:mosquitto:topic_json_test:" + device_name + "_power_channel\", persistence=\"influxdb\" }\n" \
+           + "String " + device_name + "_location \"" + device_name + " location\" { channel=\"mqtt:topic:mosquitto:topic_json_test:" + device_name + "_location_channel\", persistence=\"influxdb\" }\n" \
+           + "Number " + device_name + "_timestamp \"" + device_name + " timestamp\" { channel=\"mqtt:topic:mosquitto:topic_json_test:" + device_name + "_timestamp_channel\", persistence=\"influxdb\" }"
+    write_in_file(ITEMS_FILE_PATH, data)
