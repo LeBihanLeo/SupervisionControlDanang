c3544c174b1cdc82e7fcfca9607c5ee78c865360
Florian Latapie <florian.latapie@etu.unice.fr>
2023-07-06 12:00:07 +0700

    Update init_influxdb.sh

--- influxdb/init_influxdb.sh
+++ influxdb/init_influxdb.sh
@@ -6,4 +6,4 @@ if [[ "$(uname)" == "MINGW64_NT"* ]]; then
   start="winpty"
 fi
 
-$start docker exec -it my_influxdb influx bucket create -n $DOCKER_INFLUXDB_OPENHAB_BUCKET -o $DOCKER_INFLUXDB_INIT_ORG -r 0s
+$start docker exec -it influxdb influx bucket create -n $DOCKER_INFLUXDB_OPENHAB_BUCKET -o $DOCKER_INFLUXDB_INIT_ORG -r 0s
