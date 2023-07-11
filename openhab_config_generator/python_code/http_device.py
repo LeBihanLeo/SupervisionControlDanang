1079cc172da3e8829903a9d94852d6bae945d3d5
Benoit Gaudet <bgaudet38@gmail.com>
2023-07-11 16:39:20 +0700

    Update http_device.py

--- openhab_config_generator/python_code/http_device.py
+++ openhab_config_generator/python_code/http_device.py
@@ -2,7 +2,7 @@ from device import Device
 
 class HttpDevice(Device):
 
-    def __init__(self, device_type, device_location, device_id):
+    def __init__(self, device_type, device_location, device_id, bearer_token, ):
         filename = "bearer-http"
         Device.__init__(
             self,
@@ -13,18 +13,19 @@ class HttpDevice(Device):
             device_location,
             device_id
         )
+        self.bearer_token = bearer_token
 
     def transform_thing_file(self, input_data):
         input_data = input_data \
                      + "Thing http:url:device [\n" \
-                     + "baseURL=\"http://api.vngalaxy.vn/api/uplink/\",\n" \
-                     + "headers=\"WWW-Authenticate=Basic\",\n" \
-                     + "\"Authorization=Bearer tkoen****\",\n" \
-                     + "stateMethod=\"POST\",\n" \
-                     + "refresh=15\n" \
+                     + "    baseURL=\"http://api.vngalaxy.vn/api/uplink/\",\n" \
+                     + "    headers=\"WWW-Authenticate=Basic\",\n" \
+                     + f"    \"Authorization=Bearer {self.bearer_token}\",\n" \
+                     + "    stateMethod=\"POST\",\n" \
+                     + "    refresh=15\n" \
                      + "] {\n" \
-                     + "Channels:\n" \
-                     + "Type string : customChannel1 \"My Custom Channel\"\n" \
+                     + "    Channels:\n" \
+                     + "    Type string : customChannel1 \"My Custom Channel\"\n" \
                      + "}"
 
     def transform_item_file(self, input_data):
