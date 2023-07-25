60445b9f6cb6cd1fd5298a34b400acfbb387d31d
Thomas <billequin.thomas@gmail.com>
2023-07-25 11:22:51 +0700

    restart react project

--- openhab_config_generator/python_code/server.py
+++ openhab_config_generator/python_code/server.py
@@ -9,6 +9,13 @@ CORS(app)
 @app.route('/')
 def index():
     devices = ofh.fetch_existing_devices()
+    for existing_device in devices:
+        existing_device_type = existing_device[0]
+        existing_device_location = existing_device[1]
+        existing_device_id = existing_device[2]
+
+
+        
     my_str = jsonify(devices)
     print(my_str)
     return my_str
