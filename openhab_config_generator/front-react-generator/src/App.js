7525f629c102804ba337e3c024e35c011ab47afc
Thomas <billequin.thomas@gmail.com>
2023-07-26 14:55:17 +0700

    connexion between back end and front end

--- openhab_config_generator/front-react-generator/src/App.js
+++ openhab_config_generator/front-react-generator/src/App.js
@@ -1,15 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+   useEffect(() => {
+      fetch('http://127.0.0.1:5000/')
+         .then((res) => res.json())
+         .then((data) => {
+            console.log(data);
+            setPosts(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
   return (
     <div className="App">
       Hello
       here is our devices
 
       <div className="Box">
-      <p>Je suis un appareil</p>
-
+        <div>
+          {posts.length > 0 && (
+            <ul>
+              {posts.map(post => (
+              <li className="device">{post.device_location}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
