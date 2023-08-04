5a0618eb50536fd9b70d9cc06500f8d42b889a4a
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-08-04 10:53:17 +0700

    fix generator fake http api

--- fake_devices/http_device_generator.py
+++ fake_devices/http_device_generator.py
@@ -2,12 +2,12 @@
 # Ouvrir un fichier en mode écriture
 files = ["smart-building.things","smart-building.items"]
 for file in files:
-    content = ""
     for floor in range(1,10):
         for room in range(1,5):
             location = "f"+str(floor)+"_r"+str(room)
+            path = "/f"+str(floor)+"/r"+str(room)
             if file == "smart-building.things":
-                content += """Thing http:url:f1_r1 "f1_r1" [
+                content = """Thing http:url:f1_r1 "f1_r1" [
    baseURL="http://apps.vngalaxy.vn:5000",
     stateMethod="GET",
     refresh=30
@@ -21,13 +21,14 @@ for file in files:
 }
 """
             else:
-                content += """Number ac_f1_r1_consumption "ac_f1_r1_consumption" { channel="http:url:f1_r1:ac_f1_r1_consumption_channel", persistence="influxdb" }
+                content = """Number ac_f1_r1_consumption "ac_f1_r1_consumption" { channel="http:url:f1_r1:ac_f1_r1_consumption_channel", persistence="influxdb" }
 Number ac_f1_r1_temperature "ac_f1_r1_temperature" { channel="http:url:f1_r1:ac_f1_r1_temperature_channel", persistence="influxdb" }
 Number lights_f1_r1_consumption "lights_f1_r1_consumption" { channel="http:url:f1_r1:light_f1_r1_consumption_channel", persistence="influxdb" }
 Number lights_f1_r1_state "lights_f1_r1_state" { channel="http:url:f1_r1:light_f1_r1_state_channel", persistence="influxdb" }
 
 """         
-            content = content.replace("f1_r1", location)
-            with open("test/"+file, 'w', encoding='utf-8') as fichier:
+            content = content.replace("f1_r1", location).replace("/f1/r1", path)
+            print(len(content))
+            with open("test/"+file, 'a', encoding='utf-8') as fichier:
                 # Écrire le contenu dans le fichier
                 fichier.write(content)
